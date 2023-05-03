f8047f09f3bd1cf3ad404ea0aac51b984aec14cb
Khant Sithu <khantseithuu@gmail.com>
2023-05-03 05:02:37 +0000

    App Layout & initial sidebar

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,18 @@
 export const AppLayout = ({ children }) => {
   return (
-    <div>
-      <h1>App Layout</h1> {children}
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+        <div className="bg-indigo-500">
+          <div>Logo</div>
+          <div>cta button</div>
+          <div>tokens</div>
+        </div>
+        <div className=" flex-1 overflow-auto bg-gradient-to-b from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ">
+          list of posts
+        </div>
+        <div className="bg-emerald-500">user info - logout</div>
+      </div>
+      <div className="bg-green-200">{children}</div>
     </div>
   );
 };
