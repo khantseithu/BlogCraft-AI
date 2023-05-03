e35d4e60e946cbe034a74929ec9485e355c05955
Khant Sithu <khantseithuu@gmail.com>
2023-05-03 05:31:51 +0000

    Build CTA, coins

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,4 +1,6 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,10 +9,19 @@ export const AppLayout = ({ children }) => {
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
-        <div className="bg-indigo-500">
+        <div className="bg-indigo-500 px-2">
           <div>Logo</div>
-          <div>cta button</div>
-          <div>tokens</div>
+          <Link
+            href={"/posts/new"}
+            className="bg-green-500 w-full px-4 py-2 rounded-md text-center uppercase cursor-pointer font-bold text-white hover:bg-green-600 transition-colors block"
+          >
+            {" "}
+            New post
+          </Link>
+          <Link href={"/token-topup"} className="block mt-2 text-center ">
+            <FontAwesomeIcon icon={faCoins} className="text-yellow-500" />
+            <span className="pl-1">0 tokens available</span>
+          </Link>
         </div>
         <div className=" flex-1 overflow-auto bg-gradient-to-b from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ">
           list of posts
--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,8 +1,13 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../components/AppLayout/AppLayout";
 
 export default function TokenTopUp() {
   return <div>Token Topup</div>;
 }
+
+TokenTopUp.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
 
 export const getServerSideProps = withPageAuthRequired(() => {
   return {
